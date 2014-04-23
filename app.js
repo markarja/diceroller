6d37b1da3f5dd443ba75371c7c80e651da3ba4d4
Markus Karjalainen <info@markuskarjalainen.com>
2014-04-23 23:08:09 +0300

    Vibrate after roll.

--- app.js
+++ app.js
@@ -53,7 +53,6 @@ function roll() {
 	var start = new Date().getTime();
 	
 	playAudio("res/roll.mp3", ($("#audio").val() == 1));
-	navigator.notification.vibrate("500");
 	
 	var dice = $("#dice").val() * 1;
 	
@@ -78,6 +77,7 @@ function roll() {
 			for(var i = 1;i < dice + 1;i++) {
 				document.getElementById("die" + i).style.marginLeft = "0";
 			}
+			navigator.notification.vibrate("500");
 		};
 	}, 100);
 }

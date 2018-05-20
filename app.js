e0fdd8922b5cc32923a0390a0397af8729dbc600
Markus Karjalainen <info@markuskarjalainen.com>
2018-05-20 20:19:57 +0300

    Configuring AdMob.

--- app.js
+++ app.js
@@ -229,7 +229,6 @@ function setDiceLayout() {
 function onDeviceReady() {
 	admob.createBannerView({
 		publisherId: "ca-app-pub-1309397168819129/1051107503",
-		adSize: admob.AD_SIZE.SMART_BANNER,
 		isTesting: true,
 		offsetStatusBar: true,
 		bannerAtTop: true
